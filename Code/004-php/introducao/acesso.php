074207e05b4d0f3946c5e175e100dd4687a80f8e
Mateus Pereira <37028006+mRoxBR@users.noreply.github.com>
2019-11-01 20:00:54 -0300

    Last class and part of actual class

---
+++ Code/004-php/introducao/acesso.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Acesso</title>
+</head>
+<body>
+    <form method="post" action="validar.php">
+        <p>Nome: <input type="text" name="usuario"></p>
+        <p>Senha: <input type="password" name="senha"></p>
+        <p><input type="submit" value="Acessar"></p>
+    </form>
+</body>
+</html>
